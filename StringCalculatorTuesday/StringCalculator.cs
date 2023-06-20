7f087276b73142a56f01562bfebf2da6fa0c3b28
Qing Liang <liangqian1994@gmail.com>
2023-06-20 16:43:02 -0400

    day2-end

---
+++ StringCalculatorTuesday/StringCalculator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        return numbers =="" ? 0 : int.Parse(numbers);
+        string[] numberArray = numbers.Split(',');
+        int sum = 0;
+
+        foreach (string numberString in numberArray)
+        {
+
+            if (!string.IsNullOrEmpty(numberString))
+            {
+                sum += int.Parse(numberString);
+            }
+            
+        }
+        return sum;
+    }
+}
---
+++ StringCalculatorTuesday/StringCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void SingleNumberReturnItself()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("8");
+
+        Assert.Equal(8, result);
+    }
+
+    [Fact]
+    public void ReturnSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("2,5");
+
+        Assert.Equal(7, result);
+    }
+
+}
+
+
+
